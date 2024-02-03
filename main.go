ee11e622be2b1f2e32966d111b2f3ef05a3bdc39
Yakiyo <yakiyo1206@gmail.com>
2024-02-03 05:06:04 +0000

    chore: set log output

--- main.go
+++ main.go
@@ -1,12 +1,35 @@
 package main
 
 import (
-	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/charmbracelet/log"
 	_ "github.com/getlantern/systray"
 	_ "github.com/hugolgst/rich-go/client"
+	homedir "github.com/mitchellh/go-homedir"
 	_ "github.com/pelletier/go-toml/v2"
 )
 
+var (
+	gerr error
+	// config Config
+	hdir string
+)
+
 func main() {
-	fmt.Println("Hello World")
+	if gerr != nil {
+		return
+	}
+}
+
+func init() {
+	dir, err := homedir.Dir()
+	if err != nil {
+		gerr = err
+		return
+	}
+	hdir = dir
+	logfile := filepath.Join(hdir, ".gorp", "gorp.log")
+	log.SetOutput(os.NewFile(uintptr(os.ModePerm), logfile))
 }
