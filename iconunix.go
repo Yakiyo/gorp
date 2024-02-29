bd55ed79fc1f898f6d6fbd1273297a8f151763cc
Yakiyo <yakiyo1206@gmail.com>
2024-02-29 18:12:14 +0600

    feat: icon files

---
+++ iconunix.go
@@ -0,0 +1,8 @@
+//+build linux darwin
+
+package main
+
+import _ "embed"
+
+//go:embed assets/logo.png
+var icon []byte
---
+++ iconwin.go
@@ -0,0 +1,8 @@
+//+build windows
+
+package main
+
+import _ "embed"
+
+//go:embed assets/logo.ico
+var icon []byte
